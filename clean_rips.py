c60424760f686da527a6198071aeaff6ebafbe75
John E Jones IV <john@johnjonesfour.com>
2024-06-19 13:57:13 -0400

    Update clean_rips.py

--- clean_rips.py
+++ clean_rips.py
@@ -5,10 +5,12 @@ import shutil
 ROOT_DIR = os.environ['ROOT_DIR']
 
 for f in os.listdir(ROOT_DIR):
-    if os.path.isdir(f):
-        subdir = os.path.join(ROOT_DIR, f)
+    subdir = os.path.join(ROOT_DIR, f)
+    if os.path.isdir(subdir):        
         f1 = os.listdir(subdir)
-        if len(f1) == 1:
-            print("hit")
+        if len(f1) == 1 and f1[0].endswith('.mkv'):
+            old_path = os.path.join(subdir, f1[0])
+            new_path = os.path.join(ROOT_DIR, f"{f}.mkv")
+            print(old_path, new_path)
         else:
-            print(f1)
+            print(f"Directory {subdir} has unexpected number of files")
